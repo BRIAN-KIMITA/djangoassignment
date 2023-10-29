fa112f8998fb15a448ed1072580489b4b27c3161
BRIAN-KIMITA <briankimita21@gmail.com>
2023-10-29 14:28:53 -0700

    django assignment

---
+++ djangoprojectpractical1/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+
+def inbox(request):
+    return render(request,'index.html')
+def about(request):
+    return render(request,'about.html')
+def blog(request):
+    return render(request,'blog.html')
+def cart(request):
+    return render(request,'cart.html')
+def checkout(request):
+    return render(request,'checkout.html')
+def contact(request):
+    return render(request,'contact.html')
+def services(request):
+    return render(request,'services.html')
+def shop(request):
+    return render(request,'shop.html')
+def thankyou(request):
+    return render(request,'thankyou.html')
+def base(request):
+    return render(request,'base.html')
+
